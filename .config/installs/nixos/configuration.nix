f6475721d7b0b5f9bbc43cee6d4e5f2beecc9bae
Jeff Windsor <jeff.windsor@gmail.com>
2023-08-17 22:55:51 -0700

    Revert "layer config of nix"
    This reverts commit efb66c46fcdd022c06dde41ddd536d3b7d835502.

--- .config/installs/nixos/configuration.nix
+++ .config/installs/nixos/configuration.nix
@@ -1,25 +1,95 @@
-##################################################################################
-## FRAMEWORK 13 CONFIGURATION
-##################################################################################
 { config, pkgs, ... }:{
-  imports = [ ./configuration-development.nix ];
 
-  # System Packages || https://search.nixos.org/packages
-  environment.systemPackages = with pkgs; [
-    fwupd                                       # firmware update service
-    tlp                                         # laptop power mgmt service
+  imports = [
+    ./module/gnome.nix                          # personal module for gnome desktop
+    ./module/zsh.nix                            # personal module for zsh
+    ./hardware-configuration.nix                # hardware setup
   ];
 
-  # NixOs Options || https://search.nixos.org/options
+  # ===============================================================================================
+  #  System Packages || https://search.nixos.org/packages
+  # ===============================================================================================
+  environment.systemPackages = with pkgs; [
+    alacritty                                   # terminal improvement
+    distrobox                                   # wrapper for podman that links home to containers for easy test and dev envs
+    fwupd                                       # firmware update service
+    gcc                                         # c compiler
+    git                                         # source control
+    jetbrains-mono                              # font
+    podman                                      # container (non-root) service
+    tlp                                         # laptop power mgmt service
+    xclip                                       # terminal to clipboard manager (used by terminal and clipboard)
+  ];
+  services.flatpak.enable = true;               # allow for user installed packages via flatpak
+
+  # ===============================================================================================
+  #  NixOs Options || https://search.nixos.org/options
+  # ===============================================================================================
+
+  # == System =====================================================================================
   networking.hostName = "frame";
   time.timeZone       = "America/Los_Angeles";
   system.stateVersion = "23.05";
 
-  # Users
+  # == Users ======================================================================================
   users.users.mid = {
     isNormalUser  = true;
     description   = "The Middle Way";
     extraGroups   = [ "networkmanager" "wheel" ];
+    packages      = with pkgs; [
+      fortune                                 # saying that make my day
+      gitui                                   # source control tui
+      helix                                   # editor (kakoune like)
+      megasync                                # cloud storage
+      neovim                                  # editor (vim like)
+      yadm                                    # dotfile management
+      gnomeExtensions.forge                   # tiling window manager
+
+    ];
   };
+
+  # == Audio Services =============================================================================
+  services.pipewire = {
+    enable = true;                            # https://pipewire.org/
+    alsa.enable = true;                       # Advanced Linux Sound Architecture
+    alsa.support32Bit = true;                 #     with 32 bit application support
+    pulse.enable = true;                      # pulse audio emulation
+  };
+  hardware.pulseaudio.enable = false;         # turn off default pulse audio to use pipewire
+  security.rtkit.enable = true;               # secure real-time scheduling for user processes (recommended)
+
+  # == Boot =======================================================================================
+  boot.loader = {
+    systemd-boot.enable = true;               # EFI boot manager
+    efi.canTouchEfiVariables = true;          # installation can modify EFI boot variables
+  };
+
+  # == File Services ==============================================================================
+  boot.supportedFilesystems = [ "ntfs" ];     # NTFS for some of my USB Drives
+
+  # == Network Services ===========================================================================
+  networking.networkmanager.enable = true;    # Wifi Manager
+
+  # == Printing Services ===========================================================================
+  services.printing.enable = true;
+  # Wifi / IPP capable printing << https://nixos.wiki/wiki/Printing#IPP_everywhere_capable_printer
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;                      # for a WiFi printer
+  };
+
+  # == Upgrades and Optimization ===================================================================
+  system.autoUpgrade.enable = true;                     # auto upgrade nixos and nix packages
+  nixpkgs.config.allowUnfree = true;                    # allow more packages
+  nix.settings.auto-optimise-store = true;              # optimize links
+  nix.gc = {
+    automatic = true;                                   # garbage collection << https://nixos.wiki/wiki/Storage_optimization#Automation
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  # == Experimentals ===============================================================================
+  nix.settings.experimental-features = ["nix-command" "flakes"]; # allow flakes
 
 }
--- .config/installs/nixos/gnome.nix
+++ .config/installs/nixos/gnome.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:{
 
+  # ===============================================================================================
   # Enable GDM and GNOME DESKTOP
+  # ===============================================================================================
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
@@ -9,45 +11,59 @@
     xkbVariant = "";
   };
 
-  # Exclude Packages (From Default Gnome Packages)
+  # ===============================================================================================
+  #  Exclude Packages (From Default Gnome Packages)
+  # ===============================================================================================
   environment.gnome.excludePackages = (with pkgs; [
-    gnome-photos                      # default photo app
-    gnome-tour                        # gnome first install tour
-    gnome.atomix                      # puzzle game
-    gnome.cheese                      # webcam tool
-    gnome.epiphany                    # web browser
-    gnome.geary                       # email reader
-    gnome.gnome-contacts
-    gnome.gnome-initial-setup
-    gnome.gnome-maps
-    gnome.gnome-music
-    gnome.gnome-shell-extensions      # orginal extension manager
-    gnome.hitori                      # sudoku game
-    gnome.iagno                       # go game
-    gnome.tali                        # poker game
-    gnome.totem                       # gnome videos
-    gnome.yelp                        # Help view
-    xterm                             # good terminal, just not what I like
+    gnome-photos                # default photo app
+    gnome-tour                  # gnome first install tour
+    xterm                       # good terminal, just not what I like
+
+  # == Gnome Namespace ===================================================================
+  ]) ++ (with pkgs.gnome; [
+    atomix                      # puzzle game
+    cheese                      # webcam tool
+    epiphany                    # web browser
+    geary                       # email reader
+    gnome-contacts
+    gnome-initial-setup
+    gnome-maps
+    gnome-music
+    gnome-shell-extensions      # orginal extension manager
+    hitori                      # sudoku game
+    iagno                       # go game
+    tali                        # poker game
+    totem                       # gnome videos
+    yelp                        # Help view
+
   ]);
 
-  # Add Packages (To Personalize Gnome)
+  # ===============================================================================================
+  #  Add Packages (To Personalize Gnome)
+  # ===============================================================================================
   environment.systemPackages = (with pkgs; [
-    gnome-extension-manager               # improved extension manager
-    gnome-firmware                        # firmware application
-    gnome.gnome-terminal                  # simple backup terminal
-    gnome.seahorse                        # manage encryption keys and passwords in the GnomeKeyring
-    gnomeExtensions.appindicator          # shows old-school icons in tray (some programs need this, like mega sync)
-    gnomeExtensions.auto-move-windows     # allow the forever assignment of application to a specific desktop
-    gnomeExtensions.blur-my-shell         # add some modern blurrrr to the de
-    gnomeExtensions.caffeine              # stay awake
-    gnomeExtensions.clipman               # clipboard manager for taskbar
-    gnomeExtensions.executor              # command output to taskbar: very powerful but I only use it for fortune quotes
-    gnomeExtensions.forge                 # tiling window manager
-    gnomeExtensions.just-perfection       # tweaks to gnome visuals
-    gnomeExtensions.launch-new-instance   # when launching apps create a new instance, helps with closing only current window not all windows
-    gnomeExtensions.openweather           # weather in the taskbar
-    gnomeExtensions.space-bar             # display of workspace indicators (like i3)
-    gnomeExtensions.wallpaper-switcher    # auto switch wallpapers
-    xclip                                 # terminal to clipboard manager (used by terminal and clipboard)
+    gnome-extension-manager     # improved extension manager
+    gnome-firmware              # firmware application
+
+  # == Gnome Namespace ===================================================================
+  ]) ++ (with pkgs.gnome; [
+    gnome-terminal              # simple backup terminal
+    seahorse                    # manage encryption keys and passwords in the GnomeKeyring
+
+  # == Gnome Extensions Namepspace ========================================================
+  ]) ++ (with pkgs.gnomeExtensions; [
+    auto-move-windows           # allow the forever assignment of application to a specific desktop
+    appindicator                # shows old-school icons in tray (some programs need this, like mega sync)
+    blur-my-shell               # add some modern blurrrr to the de
+    caffeine                    # stay awake
+    clipman                     # clipboard manager for taskbar
+    executor                    # command output to taskbar: very powerful but I only use it for fortune quotes
+    just-perfection             # tweaks to gnome visuals
+    launch-new-instance         # when launching apps create a new instance, helps with closing only current window not all windows
+    openweather                 # weather in the taskbar
+    space-bar                   # display of workspace indicators (like i3)
+    wallpaper-switcher          # auto switch wallpapers
+
   ]);
+
 }
