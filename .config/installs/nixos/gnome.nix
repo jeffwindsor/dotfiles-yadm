aa50f0b88a7fba32bbde96dd4f905ed5905f77dc
Jeff Windsor <jeff.windsor@gmail.com>
2023-09-02 12:15:57 -0700

    nixos refactor

--- .config/installs/nixos/gnome.nix
+++ .config/installs/nixos/gnome.nix
@@ -17,51 +17,50 @@
   # add the games pack
   services.gnome.games.enable = true;
 
-  # preferred packages
   environment.systemPackages = (with pkgs; [
+    # preferred packages
     gnome-extension-manager             # improved extension manager
     gnome-firmware                      # firmware application
     xclip                               # manages terminal to clipboard copies
     gnome.gnome-terminal                # simple backup terminal
     gnome.seahorse                      # manage encryption keys and passwords in the GnomeKeyring
-  ]};
 
-  # preferred gnome extensions
-  environment.systemPackages = (with pkgs.gnomeExtensions; [
-    auto-move-windows                   # allow the forever assignment of application to a specific desktop
-    appindicator                        # shows old-school icons in tray (some programs need this, like mega sync)
-    blur-my-shell                       # add some modern blurrrr to the de
-    caffeine                            # stay awake
-    clipman                             # clipboard manager for taskbar
-    executor                            # command output to taskbar: very powerful but I only use it for fortune quotes
-    forge                               # tiling window manager
-    just-perfection                     # tweaks to gnome visuals
-    launch-new-instance                 # when launching apps create a new instance, helps with closing only current window not all windows
-    openweather                         # weather in the taskbar
-    space-bar                           # display of workspace indicators (like i3)
-    wallpaper-switcher                  # auto switch wallpapers
-    pano                                # Next-gen Clipboard manager for Gnome Shell
+    # preferred gnome extensions
+    gnomeExtensions.auto-move-windows   # allow the forever assignment of application to a specific desktop
+    gnomeExtensions.appindicator        # shows old-school icons in tray (some programs need this, like mega sync)
+    gnomeExtensions.blur-my-shell       # add some modern blurrrr to the de
+    gnomeExtensions.caffeine            # stay awake
+    gnomeExtensions.clipman             # clipboard manager for taskbar
+    gnomeExtensions.executor            # command output to taskbar: very powerful but I only use it for fortune quotes
+    gnomeExtensions.forge               # tiling window manager
+    gnomeExtensions.just-perfection     # tweaks to gnome visuals
+    gnomeExtensions.launch-new-instance # when launching apps create a new instance, helps with closing only current window not all windows
+    gnomeExtensions.openweather         # weather in the taskbar
+    gnomeExtensions.space-bar           # display of workspace indicators (like i3)
+    gnomeExtensions.wallpaper-switcher  # auto switch wallpapers
+    gnomeExtensions.pano                # Next-gen Clipboard manager for Gnome Shell
   ]);
 
   # ===============================================================================================
   #  Exclusions
   # ===============================================================================================
   environment.gnome.excludePackages = (with pkgs; [
-    # gnome-photos                      # default photo app
-    gnome-tour                        # gnome first install tour
-    gnome.cheese                      # webcam tool
-    gnome.epiphany                    # web browser
-    # gnome.geary                       # email reader
-    # gnome.gnome-contacts
+    gnome-tour                          # gnome first install tour
+    gnome.cheese                        # webcam tool
+    gnome.epiphany                      # web browser
     gnome.gnome-initial-setup
     gnome.gnome-maps
     gnome.gnome-music
-    gnome.totem                       # gnome videos
-    gnome.yelp                        # Help view
+    gnome.totem                         # gnome videos
+    gnome.yelp                          # Help view
+    xterm
+
+    # gnome-photos                        # default photo app
+    # gnome.geary                         # email reader
+    # gnome.gnome-contacts
   ]);
 
   # remove xterm, good terminal, just not what I like
-  services.xserver.desktopManager.xterm.enable = false    # required to remove xterm
-  environment.gnome.excludePackages = [ pkgs.xterm ];
+  services.xserver.desktopManager.xterm.enable = false;    # required to remove xterm
 
 }
