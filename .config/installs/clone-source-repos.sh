c43467758c22f03c337d3d928790dc64df20d1ff
Jeff Windsor <jeff.windsor@gmail.com>
2023-09-10 14:48:06 -0700

    add nixos-config

--- .config/installs/clone-source-repos.sh
+++ .config/installs/clone-source-repos.sh
@@ -3,3 +3,4 @@
 echo "\n==> Cloning Personal Github Repos"
 git clone git@github.com:jeffwindsor/financials.git "$HOME"/Source/github.com/jeffwindsor/financials
 git clone git@github.com:jeffwindsor/startpage.git "$HOME"/Source/github.com/jeffwindsor/startpage
+git clone git@github.com:jeffwindsor/nixos-config.git "$HOME"/Source/github.com/jeffwindsor/nixos-config
