bb88fac2b8c3c9a04e30b29c504c0235eab7491f
Jeff Windsor <jeff.windsor@gmail.com>
2023-09-11 20:11:05 -0700

    back to system packages

--- .config/installs/nixos/configuration.nix
+++ .config/installs/nixos/configuration.nix
@@ -22,114 +22,37 @@
     ];
 
     systemPackages = with pkgs; [
-      cups-brother-hll2350dw                  # home and office printer (2023)
-      fwupd                                   # firmware update service
-      tlp                                     # laptop power mgmt service
-    ];
-
-  };
-
-  hardware.pulseaudio.enable = false;         # AUDIO: turn off default pulse audio to use pipewire
-
-  networking= {
-    hostName              = "frame";
-    networkmanager.enable = true;             # Wifi Manager
-  };
-
-  nixpkgs.config.allowUnfree = true;          # allow more packages
-
-  nix = {
-    settings = {
-      auto-optimise-store   = true;
-      experimental-features = [ "nix-command" "flakes" ];
-    };
-    gc = {
-      automatic = true;                       # https://nixos.wiki/wiki/Storage_optimization#Automation
-      dates     = "weekly";
-      options   = "--delete-older-than 7d";
-    };
-  };
-
-  programs = {
-    zsh = {                                   # ZSH
-      enable                    = true;
-      autosuggestions.enable    = true;
-      syntaxHighlighting.enable = true;
-    };
-  };
-
-  security.rtkit.enable = true;               # AUDIO: used by pipewire
-
-  services = {
-
-    avahi = {                                 # PRINTING: service discovery on a local network
-      enable        = true;
-      nssmdns       = true;
-      openFirewall  = true;                   # Wifi printing
-    };
-
-    flatpak ={
-      enable = true;                          # allow for user installed packages via flatpak
-    };
-
-    pipewire = {                              # AUDIO: sound services
-      enable            = true;
-      alsa.enable       = true;
-      alsa.support32Bit = true;
-      pulse.enable      = true;
-    };
-
-    printing = {                              # PRINTING: standard printing services
-      enable  = true;
-      drivers = [ pkgs.cups-brother-hll2350dw ];
-    };
-
-  };
-
-  system = {
-    autoUpgrade.enable  = true;               # auto upgrade nixos and nix packages
-    stateVersion        = "23.05";
-  };
-
-  time.timeZone = "America/Los_Angeles";
-
-  users.users.mid = {
-    isNormalUser  = true;
-    description   = "The Middle Way";
-    extraGroups   = [ "networkmanager" "wheel" ];
-    shell         = pkgs.zsh;
-    packages      = with pkgs; [
       # applications
       alacritty                               # terminal improvement
-      brave                                   # experimental: browser
-      chromium                                # browser backup
-      firefox                                 # browser main
-      helix                                   # experimental: editor (kakoune like)
-      librewolf                               # experimental: browser
+      brave                                   # maybe keep: browser
+      chromium                                # browser (backup)
+      firefox                                 # browser (main)
+      helix                                   # maybe keep: editor (kakoune like)
+      librewolf                               # maybe keep: browser
       megasync                                # cloud storage
       neovim                                  # editor (vim like)
 
       # tools
       bat                                     # cat replacement
       exa                                     # ls replacement
-      fd                                      # experimental: find replacement
+      fd                                      # maybe keep: find replacement
       fortune                                 # saying that make my day
-      freshfetch                              # experimental: neofetch replacement
+      freshfetch                              # maybe keep: neofetch replacement
       fzf                                     # fuzzy finder
       gcc                                     # c compiler
       git                                     # source control
-      lazygit                                 # experimental: tui git client
+      lazygit                                 # maybe keep: tui git client
       ripgrep                                 # grep replacement
-      sd                                      # experimental: sed replacement
+      sd                                      # maybe keep: sed replacement
       starship                                # prompt
       tealdeer                                # tldr replacement
-      xh                                      # experimental: curl replacement
-      xplr                                    # experimental: tui file explorer
+      xh                                      # maybe keep: curl replacement
+      xplr                                    # maybe keep: tui file explorer
       yadm                                    # dotfile management
-      zellij                                  # experimental: tmux replacement
+      zellij                                  # maybe keep: tmux replacement
 
       # shells
-      nushell                                 # experimental: modern shell written in Rust
+      nushell                                 # maybe keep: modern shell written in Rust
       zsh                                     # main shell
       zsh-autosuggestions
       zsh-syntax-highlighting
@@ -138,6 +61,91 @@
       jetbrains-mono                          # main font
       lexend
       nerdfonts
+
+      # system
+      cups-brother-hll2350dw                  # home and office printer (2023)
+      fwupd                                   # firmware update service
+      tlp                                     # laptop power mgmt service
+    ];
+
+  };
+
+  # AUDIO: turn off default pulse audio to use pipewire
+  hardware.pulseaudio.enable = false;
+
+  networking= {
+    hostName = "frame";
+    networkmanager.enable = true;             # Wifi Manager
+  };
+
+  # remove unfree check
+  nixpkgs.config.allowUnfree = true;
+
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+
+    # GARBAGE COLLECTION: https://nixos.wiki/wiki/Storage_optimization#Automation
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  programs = {
+    zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+    };
+  };
+
+  # AUDIO: used by pipewire
+  security.rtkit.enable = true;
+
+  services = {
+    # PRINTING: service discovery on a local network
+    avahi = {
+      enable = true;
+      nssmdns = true;
+      openFirewall = true;                  # Wifi printing
+    };
+
+    # FLATPAK: user installeble
+    flatpak.enable = true;
+
+    # AUDIO: sound services
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    # PRINTING: standard printing services
+    printing = {
+      enable = true;
+      drivers = [ pkgs.cups-brother-hll2350dw ];
+    };
+
+  };
+
+  system = {
+    autoUpgrade.enable = true;
+    stateVersion = "23.05";
+  };
+
+  time.timeZone = "America/Los_Angeles";
+
+  users.users.mid = {
+    isNormalUser = true;
+    description = "The Middle Way";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
     ];
   };
 
