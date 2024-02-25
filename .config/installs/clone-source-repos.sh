0c5a9d49ebffdf756d147a27b8a048014952de24
Jeff Windsor <jeff.windsor@gmail.com>
2024-02-24 19:50:02 -0800

    removed installs. skhd. tmux and zellij

--- .config/installs/clone-source-repos.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "\n==> Cloning Personal Github Repos"
-git clone git@github.com:jeffwindsor/financials.git "$HOME"/Source/github.com/jeffwindsor/financials
-git clone git@github.com:jeffwindsor/startpage.git "$HOME"/Source/github.com/jeffwindsor/startpage
-git clone git@github.com:jeffwindsor/nixos-config.git "$HOME"/Source/github.com/jeffwindsor/nixos-config
