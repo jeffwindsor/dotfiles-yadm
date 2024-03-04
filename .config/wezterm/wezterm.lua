32afb35cc9391d80653388f6ab0d9f22053cd364
Jeff Windsor <jeff.windsor@gmail.com>
2024-03-04 07:14:16 -0800

    'Refactor'

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,16 +6,16 @@ local config = wezterm.config_builder()
 
 -- =============================================================
 -- COLOR SCHEME: DARK
--- config.color_scheme = 'Bitmute (terminal.sexy)'
+-- config.color_scheme = "Bitmute (terminal.sexy)"
 -- config.color_scheme = 'Batman'
--- config.color_scheme = 'Gruvbox Dark (Gogh)'
--- config.color_scheme = 'Kanagawa (Gogh)'
--- config.color_scheme = 'Nord (Gogh)'
--- config.color_scheme = 'OceanicNext (base16)'
+config.color_scheme = "Gruvbox Dark (Gogh)"
+-- config.color_scheme = "Kanagawa (Gogh)"
+-- config.color_scheme = "Nord (Gogh)"
+-- config.color_scheme = "OceanicNext (base16)"
 -- config.color_scheme = "Solarized (dark) (terminal.sexy)"
 -- config.color_scheme = "Tender (Gogh)"
 -- config.color_scheme = 'Tokyo Night (Gogh)'
-config.color_scheme = "Tokyo Night Storm (Gogh)"
+-- config.color_scheme = "Tokyo Night Storm (Gogh)"
 
 -- COLOR SCHEME: LIGHT
 -- config.color_scheme = 'Solarized (light) (terminal.sexy)'
