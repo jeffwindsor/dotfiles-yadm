d1939020f270aafee804b593c654afbe791363c5
Jeff Windsor <jeff.windsor@gmail.com>
2024-04-01 14:54:23 -0700

    Wezterm Config added colors

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local act = wezterm.action
 
 -- =============================================================
 
-config.color_scheme = "Tokyo Night Storm (Gogh)"
+config.color_scheme = 'tender (base16)'
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 14.0
 config.window_background_opacity = 0.98
