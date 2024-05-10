2e0f960325b63e360c93fe10dacef3d1ae5a70d9
Jeff Windsor <jeff.windsor@gmail.com>
2024-05-09 20:29:21 -0700

    resplit out plugin configs

---
+++ .config/nvim/lua/plugins/lazyvim.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "tokyonight-night" },
+  },
+  -- CODING
+  -- { "hrsh7th/nvim-cmp", enabled = false },
+  -- { "L3MON4D3/LuaSnip", enabled = false },
+  -- { "rafamadriz/friendly-snippets", enabled = false },
+  -- { "saadparwaiz1/cmp_luasnip", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  -- { "echasnovski/mini.surround", enabled = false },
+  -- { "JoosepAlviste/nvim-ts-context-commentstring", enabled = false },
+  -- { "echasnovski/mini.ai", enabled = false },
+
+  -- EDITOR
+  -- { "folke/flash.nvim", enabled = false },
+  -- { "folke/todo-comments.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- { "nvim-pack/nvim-spectre", enabled = false },
+  -- {   "lewis6991/gitsigns.nvim", enabled = false },
+  -- {   "RRethy/vim-illuminate", enabled = false },
+  -- {   "echasnovski/mini.bufremove", enabled = false },
+  -- {   "folke/trouble.nvim", enabled = false },
+
+  -- UI
+  -- { "folke/noice.nvim", enabled = false },
+  -- { "stevearc/dressing.nvim", enabled = false },
+  -- "rcarriga/nvim-notify",
+  -- "akinsho/bufferline.nvim",
+  -- "nvim-lualine/lualine.nvim",
+  -- "lukas-reineke/indent-blankline.nvim",
+  -- "echasnovski/mini.indentscope",
+  -- "MunifTanjim/nui.nvim",
+  -- "nvim-tree/nvim-web-devicons",
+  -- "goolord/alpha-nvim",
+
+  -- LSP
+}
