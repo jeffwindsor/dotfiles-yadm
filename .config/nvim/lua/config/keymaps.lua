78c216216736ea2705410c1d6d5a14e89631e8f6
Jeff Windsor <jeff.windsor@gmail.com>
2024-05-15 17:23:25 -0700

    Nvim Oil keymap consolidation
    Also cleaned up files and names

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,3 @@
 -- Add any additional keymaps here
 --
 --vim.keymap.set
--- vim.keymap.set("n", "<leader>cfx", "<cmd>%!xmllint --format - <cr>", { desc = "Format Selected XML" })
--- vim.keymap.set("n", "<leader>y", "", { desc = "+Other" })
-vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open current directory" })
--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -4,7 +4,23 @@ return {
     -- cmd = "Oil",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("oil").setup({ default_file_explorer = true })
+      require("oil").setup({
+        default_file_explorer = true,
+        columns = { "icon" },
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<M-h>"] = "actions.select_split",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      })
+
+      -- Open parent directory in current window
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+      -- Open parent directory in floating window
+      vim.keymap.set("n", "_", require("oil").toggle_float, { desc = "Open in Floating Window" })
     end,
   },
 }
