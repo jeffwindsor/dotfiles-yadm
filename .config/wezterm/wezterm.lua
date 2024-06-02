aaaa2b28d81d114f4a5789c7a4ae9fd3b07b1d4c
Jeff Windsor <jeff.windsor@gmail.com>
2024-06-02 14:42:46 -0700

    Wezterm - Added Direct Pane Management

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local act = wezterm.action
 
 -- =============================================================
 
-config.color_scheme = 'Tokyo Night Moon'
+config.color_scheme = "Tokyo Night Moon"
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 14.0
 config.window_background_opacity = 0.98
@@ -13,43 +13,62 @@ config.window_close_confirmation = "NeverPrompt"
 -- =============================================================
 
 config.keys = {
-	-- ENTER PANE MANAGEMENT KEY MODE
-	{
-		key = "Space",
-		mods = "CTRL|ALT|SUPER",
-		action = act.ActivateKeyTable({
-			name = "pane_management",
-			one_shot = false,
-		}),
-	},
+	-- PANES
+	--  CLOSE
+	{ key = "x", mods = "CTRL", action = act.CloseCurrentPane({ confirm = false }) }, -- CLOSE
+	--  MOVE
+	{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+	--  SPLIT
+	{ key = "h", mods = "CTRL|ALT", action = act.SplitPane({ direction = "Left" }) },
+	{ key = "k", mods = "CTRL|ALT", action = act.SplitPane({ direction = "Up" }) },
+	{ key = "l", mods = "CTRL|ALT", action = act.SplitPane({ direction = "Right" }) },
+	{ key = "j", mods = "CTRL|ALT", action = act.SplitPane({ direction = "Down" }) },
+	--  RESIZE
+	{ key = "h", mods = "CTRL|ALT|SUPER", action = act.AdjustPaneSize({ "Left", 1 }) },
+	{ key = "l", mods = "CTRL|ALT|SUPER", action = act.AdjustPaneSize({ "Right", 1 }) },
+	{ key = "k", mods = "CTRL|ALT|SUPER", action = act.AdjustPaneSize({ "Up", 1 }) },
+	{ key = "j", mods = "CTRL|ALT|SUPER", action = act.AdjustPaneSize({ "Down", 1 }) },
+
+	-- {
+	-- 	key = "p",
+	-- 	mods = "CTRL|ALT|SHIFT",
+	-- 	action = act.ActivateKeyTable({
+	-- 		name = "pane_management",
+	-- 		one_shot = false,
+	-- 	}),
+	-- },
 }
 
-config.key_tables = {
-	pane_management = {
-
-		{ key = "Escape", action = "PopKeyTable" }, -- CANCEL MODE
-		{ key = "q", action = act.CloseCurrentPane({ confirm = false }) }, -- CLOSE
-		{ key = "x", action = act.CloseCurrentPane({ confirm = false }) }, -- CLOSE
-
-		-- ACTIVATE
-		{ key = "h", action = act.ActivatePaneDirection("Left") },
-		{ key = "l", action = act.ActivatePaneDirection("Right") },
-		{ key = "k", action = act.ActivatePaneDirection("Up") },
-		{ key = "j", action = act.ActivatePaneDirection("Down") },
-
-		-- SPLIT
-		{ key = "h", mods = "CTRL", action = act.SplitPane({ direction = "Left" }) },
-		{ key = "k", mods = "CTRL", action = act.SplitPane({ direction = "Up" }) },
-		{ key = "l", mods = "CTRL", action = act.SplitPane({ direction = "Right" }) },
-		{ key = "j", mods = "CTRL", action = act.SplitPane({ direction = "Down" }) },
-
-		-- RESIZE
-		{ key = "h", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "l", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Right", 1 }) },
-		{ key = "k", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "j", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Down", 1 }) },
-	},
-}
+-- config.key_tables = {
+-- 	-- PANE MANAGEMENT MODE KEY MAP
+-- 	pane_management = {
+--
+-- 		{ key = "Escape", action = "PopKeyTable" }, -- CANCEL MODE
+-- 		{ key = "q", action = act.CloseCurrentPane({ confirm = false }) }, -- CLOSE
+-- 		{ key = "x", action = act.CloseCurrentPane({ confirm = false }) }, -- CLOSE
+--
+-- 		-- ACTIVATE
+-- 		{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+-- 		{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+-- 		{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+-- 		{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+--
+-- 		-- SPLIT
+-- 		{ key = "h", mods = "CTRL|ALT", action = act.SplitPane({ direction = "Left" }) },
+-- 		{ key = "k", mods = "CTRL|ALT", action = act.SplitPane({ direction = "Up" }) },
+-- 		{ key = "l", mods = "CTRL|ALT", action = act.SplitPane({ direction = "Right" }) },
+-- 		{ key = "j", mods = "CTRL|ALT", action = act.SplitPane({ direction = "Down" }) },
+--
+-- 		-- RESIZE
+-- 		{ key = "h", mods = "CTRL|ALT|SUPER", action = act.AdjustPaneSize({ "Left", 1 }) },
+-- 		{ key = "l", mods = "CTRL|ALT|SUPER", action = act.AdjustPaneSize({ "Right", 1 }) },
+-- 		{ key = "k", mods = "CTRL|ALT|SUPER", action = act.AdjustPaneSize({ "Up", 1 }) },
+-- 		{ key = "j", mods = "CTRL|ALT|SUPER", action = act.AdjustPaneSize({ "Down", 1 }) },
+-- 	},
+-- }
 
 -- =============================================================
 
