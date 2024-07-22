1396b155bcdf4d9702cb6e716f22b8edadf7d141
Windsor, Jeff <jeffrey.windsor@cj.com>
2024-07-22 07:20:38 -0700

    Fortune - fix multiple locations

--- Library/Application Support/xbar/plugins/fortune.1m.sh
+++ Library/Application Support/xbar/plugins/fortune.1m.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env sh
-/opt/homebrew/bin/fortune $HOME/.config/fortune/quotes
+if test -f /usr/local/bin/fortune; then
+  /usr/local/bin/fortune $HOME/.config/fortune/quotes
+elif test -f /opt/homebrew/bin/fortune; then
+  /opt/homebrew/bin/fortune $HOME/.config/fortune/quotes
+else
+  echo "cannot find fortune"
+fi
