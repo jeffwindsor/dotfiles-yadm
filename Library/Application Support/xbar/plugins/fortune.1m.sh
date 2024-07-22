e71e163db27af1fb0c4c72413b96a109b87a4fe8
Jeff Windsor <jeff.windsor@gmail.com>
2024-07-22 10:28:07 -0700

    Fortune - move to folder use

--- Library/Application Support/xbar/plugins/fortune.1m.sh
+++ Library/Application Support/xbar/plugins/fortune.1m.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
+
+# execute fortune based on homebrew location
 if test -f /usr/local/bin/fortune; then
-  /usr/local/bin/fortune $HOME/.config/fortune/quotes
+  /usr/local/bin/fortune $HOME/.config/fortune/
 elif test -f /opt/homebrew/bin/fortune; then
-  /opt/homebrew/bin/fortune $HOME/.config/fortune/quotes
+  /opt/homebrew/bin/fortune $HOME/.config/fortune/
 else
   echo "cannot find fortune"
 fi
