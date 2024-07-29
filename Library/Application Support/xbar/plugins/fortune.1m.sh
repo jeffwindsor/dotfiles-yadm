2e311ea62b291368c6a0d9ff9a7952a7e6492d24
Jeff Windsor <jeff.windsor@gmail.com>
2024-07-29 11:38:51 -0700

    Xbar - use fold and length limit

--- Library/Application Support/xbar/plugins/fortune.1m.sh
+++ Library/Application Support/xbar/plugins/fortune.1m.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
 
-# execute fortune based on homebrew location
+length="25"
+
 quote="cannot find fortune executable"
 
+# execute fortune based on homebrew location
 if test -f /usr/local/bin/fortune; then
   quote=$(/usr/local/bin/fortune $HOME/.config/fortune/)
 elif test -f /opt/homebrew/bin/fortune; then
   quote=$(/opt/homebrew/bin/fortune $HOME/.config/fortune/)
 fi
 
-echo "$quote | length=75"
+quote=$(echo $quote | fold -w $length -s)
+
+echo "$quote | length=$length"
 echo "---"
 echo "$quote"
