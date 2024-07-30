fea3c32064f999e46ed67a429c188d1f3c479a3f
Jeff Windsor <jeff.windsor@gmail.com>
2024-07-29 20:59:24 -0700

    NVIM - Dashboard and Dotsfile Search

--- .config/nvim/lua/plugins/dashboard-nvim.lua
+++ .config/nvim/lua/plugins/dashboard-nvim.lua
@@ -23,6 +23,40 @@ return {
         "neovim",
         "",
       }
+      opts.config.center = {
+        { action = "lua LazyVim.pick()()", desc = " Find File", icon = " ", key = "f" },
+        { action = "ene | startinsert", desc = " New File", icon = " ", key = "n" },
+        { action = 'lua LazyVim.pick("oldfiles")()', desc = " Recent Files", icon = " ", key = "r" },
+        { action = 'lua LazyVim.pick("live_grep")()', desc = " Find Text", icon = " ", key = "g" },
+        { action = "lua LazyVim.pick.config_files()()", desc = " Config", icon = " ", key = "c" },
+        {
+          action = function()
+            vim.api.nvim_input("<cmd>Telescope find_files find_command=yadm,list,-a<cr>")
+          end,
+          desc = " Dotfiles",
+          icon = " ",
+          key = "d",
+        },
+        { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
+        { action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "x" },
+        { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
+        {
+          action = function()
+            vim.api.nvim_input("<cmd>qa<cr>")
+          end,
+          desc = " Quit",
+          icon = " ",
+          key = "q",
+        },
+      }
+
+      -- Clean up the presentation of the dashboard items
+      for _, button in ipairs(opts.config.center) do
+        -- expand width of descriptions
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        -- Show only key, no [ ]
+        button.key_format = "  %s"
+      end
     end,
   },
 }
