6e3cbcfccb129597e6434c7384df2b7ae2cc5128
Jeff Windsor <jeff.windsor@gmail.com>
2024-08-02 22:53:10 -0700

    Nix Darwin - include brew casks

--- .config/installs/darwin-configuration.nix
+++ .config/installs/darwin-configuration.nix
@@ -5,26 +5,43 @@
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [ 
-      pkgs.bat
-      pkgs.eza
-      pkgs.fastfetch
-      pkgs.fd
-      pkgs.fortune
-      pkgs.fzf
-      pkgs.git
-      pkgs.lazygit
-      pkgs.neovim
-      pkgs.ripgrep
-      pkgs.sd
-      pkgs.starship
-      pkgs.tldr
-      pkgs.yadm
-      pkgs.yazi
-      pkgs.zoxide
-      pkgs.zsh
-    ];
+  environment.systemPackages = with pkgs; [ 
+    bat
+    eza
+    fastfetch
+    fd
+    fortune
+    fzf
+    git
+    lazygit
+    neovim
+    ripgrep
+    sd
+    starship
+    tldr
+    yadm
+    yazi
+    zoxide
+    zsh
+  ];
+
+  fonts.packages = with pkgs; [
+    jetbrains-mono
+    nerdfonts
+  ];
+
+  homebrew.enable = true;
+  homebrew.casks = [
+    aerospace
+    balenaetcher
+    firefox
+    google-chrome
+    keepingyouawake
+    spotify
+    transmission
+    vlc
+    wezterm
+  ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
