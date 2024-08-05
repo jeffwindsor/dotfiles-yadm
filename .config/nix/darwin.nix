643d2a6b74a0178ce1795c9c123c06397bc1cbf5
Jeff Windsor <jeff.windsor@gmail.com>
2024-08-04 21:22:55 -0700

    Nix - Darwin Config Working Flake

--- .config/nix/darwin.nix
+++ .config/nix/darwin.nix
@@ -1,6 +1,33 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }:{
+  environment.darwinConfig = "$XDG_CONFIG_HOME/nix";
 
-{
+  # List packages: nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [ 
+    bat
+    eza
+    fastfetch
+    fd
+    fortune
+    fzf
+    git
+    lazygit
+    neovim
+    ripgrep
+    sd
+    starship
+    tldr
+    yadm
+    yazi
+    zoxide
+    zsh
+  ];
+
+  fonts.packages = with pkgs; [
+    jetbrains-mono
+    lexend
+    nerdfonts
+  ];
+
   homebrew.enable = true;
   homebrew.casks = [
     "aerospace"
@@ -16,11 +43,20 @@
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  # system.configurationRevision = self.rev or self.dirtyRev or null;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+
 }
--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -1,69 +1,37 @@
 {
-  description = "NixOS, NixDarwin and Home Manager configuration";
+  description = "NixOS and Darwin flake - Jeff Windsor";
 
-  inputs = {
+  inputs = { 
+
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     };
+
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     home-manager = {
-      url = "github:nix-community/home-manager";
-     inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile: nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs; [ 
-        bat
-        eza
-        fastfetch
-        fd
-        fortune
-        fzf
-        git
-        lazygit
-        neovim
-        ripgrep
-        sd
-        starship
-        tldr
-        yadm
-        yazi
-        zoxide
-        zsh
-      ];
-
-      fonts.packages = with pkgs; [
-        jetbrains-mono
-        nerdfonts
-      ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-    };
-  in
+  outputs = { self, nixpkgs, nix-darwin, home-manager, ...}@inputs:
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#midnight
+    # $ darwin-rebuild build --flake .#Midnight-Air
     darwinConfigurations."Midnight-Air" = nix-darwin.lib.darwinSystem {
+      # The platform the configuration will be used on.
       system = "aarch64-darwin";
       modules = [ 
-        ./darwin.nix
+        ./darwin.nix 
       ];
-
     };
 
     # Expose the package set, including overlays, for convenience.
-    # darwinPackages = self.darwinConfigurations."Midnight-Air".pkgs;
+    darwinPackages = self.darwinConfigurations."Midnight-Air".pkgs;
   };
 }
