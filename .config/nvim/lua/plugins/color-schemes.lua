9ba2e95507a8232dc8ccac5e020bb5825a2971da
Jeff Windsor <jeff.windsor@gmail.com>
2024-08-10 07:23:46 -0700

    NVIM - Transparent

--- .config/nvim/lua/plugins/color-schemes.lua
+++ .config/nvim/lua/plugins/color-schemes.lua
@@ -1,6 +1,15 @@
 return {
 
-  { "folke/tokyonight.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
   { "jacoborus/tender.vim" },
   { "navarasu/onedark.nvim" },
   { "rebelot/kanagawa.nvim" },
