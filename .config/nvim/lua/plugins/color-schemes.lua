c12c4af27a0b6fb2cc939e24be0404f3d2c34375
Jeff Windsor <jeff.windsor@gmail.com>
2024-08-10 11:51:09 -0700

    Nvim - Colorschemes

--- .config/nvim/lua/plugins/color-schemes.lua
+++ .config/nvim/lua/plugins/color-schemes.lua
@@ -3,27 +3,41 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
+      -- transparency
       transparent = true,
       styles = {
         sidebars = "transparent",
         floats = "transparent",
       },
+
+      -- visibility
+      on_highlights = function(hl, c)
+        -- increase visibility of window separators (horz & vert)
+        hl.WinSeparator = { bold = true, fg = c.cyan }
+        -- increase visibility of line numbers
+        hl.CursorLineNr = { bold = true, fg = c.orange }
+        hl.LineNr = { fg = c.orange }
+        hl.LineNrAbove = { fg = c.comment }
+        hl.LineNrBelow = { fg = c.comment }
+      end,
     },
   },
+  -- { "AlexvZyl/nordic.nvim" },
+  -- { "Daiki48/sakurajima.nvim" },
+  -- { "bluz71/vim-nightfly-colors" },
+  -- { "loctvl842/monokai-pro.nvim" },
+  -- { "sainnhe/everforest" },
+  -- { "NTBBloodbath/doom-one.nvim" },
   { "jacoborus/tender.vim" },
   { "navarasu/onedark.nvim" },
   { "rebelot/kanagawa.nvim" },
-
-  -- Trying
-  -- { "bluz71/vim-nightfly-colors" },
-  { "sainnhe/everforest" },
-  -- { "Daiki48/sakurajima.nvim" },
-  -- { "loctvl842/monokai-pro.nvim" },
-  -- { "AlexvZyl/nordic.nvim" },
+  -- { "rmehri01/onenord.nvim" },
   { "shaunsingh/nord.nvim" },
 
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "tokyonight" },
+    opts = {
+      colorscheme = "tokyonight-moon",
+    },
   },
 }
